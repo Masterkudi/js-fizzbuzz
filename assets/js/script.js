c480572a82a921aafcc56bf21c0439938d75742d
masterkudi <cudini.andrea@gmail.com>
2023-07-05 11:03:22 +0200

    correzione in classe, esercizio completato

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,55 +4,45 @@
 const chessContainer = document.querySelector('.chess-container');
 const totalCells = 10 * 10;
 
-for (let i = 0; i < totalCells; i++) {
-    const singleCell = `<div class="chess-square"></div>`;
-    //let singleCell = document.createElement('div');
+for (let i = 1; i <= totalCells; i++) {
+    let singleCell = document.createElement('div');
     singleCell.classList.add('chess-square');
-    chessContainer.innerHTML = i.toString();
+    
+    let output = '';
 
+    // Controllo multipli di 3
+    if (i % 3 === 0) {
+      output += 'Fizz';
+    }
+  
+    // Controllo multipli di 5
+    if (i % 5 === 0) {
+      output += 'Buzz';
+    }
+  
+    // Controllo se output è vuoto
+    if (output === '') {
+      output = i;
+    }
 
-    const container = document.createElement('chess-square');
-    chessContainer.append(singleCell);
+    singleCell.innerHTML = output
 
-}
-
-
-
-// Ciclo da 1 a 100
-for (let i = 1; i <= 100; i++) {
-  let output = '';
-
-  // Controllo multipli di 3
-  if (i % 3 === 0) {
-    output += 'Fizz';
-  }
-
-  // Controllo multipli di 5
-  if (i % 5 === 0) {
-    output += 'Buzz';
-  }
-
-  // Controllo se output è vuoto
-  if (output === '') {
-    output = i;
-  }
-
-
-  // Creazione dell'elemento HTML
-  const element = document.createElement('div');
-  element.textContent = output;
-  container.appendChild(element);
-
-  // Aggiunta degli stili
+    // Aggiunta degli stili
+    
   if (output === 'Fizz') {
     singleCell.classList.add('fizz');
   } else if (output === 'Buzz') {
     singleCell.classList.add('buzz');
   } else if (output === 'FizzBuzz') {
-    singleCell.classList.add('fizz buzz');
+    singleCell.classList.add('fizzbuzz');
   } else {
     singleCell.classList.add('number');
   }
+
+
+    chessContainer.appendChild(singleCell);
+
 }
+
 
 
